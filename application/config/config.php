d8d813c43ab72c0747b79bbeb852913dfcd3754f
root <root@skripsi.fxky2u5uu1vercerl4afqvwa2b.ix.internal.cloudapp.net>
2019-11-24 12:39:12 +0000

    berubah dikit

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //config base_url
-$config['base_url']	= "http://".$_SERVER['HTTP_HOST'].
-str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url']	="";
 
 /*
 
@@ -56,7 +55,7 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol']	= 'AUTO';
 
 /*
 |--------------------------------------------------------------------------
